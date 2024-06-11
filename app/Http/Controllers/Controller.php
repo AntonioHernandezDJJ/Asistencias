80bc76ea05d8fa57ae30e1f63825c1af78388e3e
IngAntonio13 <antojth1314@gmail.com>
2024-06-11 16:05:28 -0600

    Guardado
    guardado

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,23 +4,6 @@ namespace App\Http\Controllers;
 
 class UserContoller extends Controller 
 {
-       public function ver(){
-       // Array de usuarios
-       $usuario = [];
-       // For que genere 10 usuarios
-       for ($i=0; $i < 10; $i++) {
-       // Crear un usuario
-           $usuario = new (stdClass());
-          // Asignar un nombre
-           $usuariouario->nombre = 'Usuario ' * $i;
-          // Asignar un correo
-           $usuario>$correo = 'usuario' * $i * '@gmail.com';
-          // Agregar el usuario al array
-           [$usuariosl] = $usuario;
-          // Sirve para ver el contenido de la variable
-          //dd ($usuarios);
-          // Retornar la vista con los usuarios
-          return view( 'usuarios.vista', compact( 'usuarios'));
-          }
+   
 }
-}
+
